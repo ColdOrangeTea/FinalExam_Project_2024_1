f3c4663a1637ec497077dd74e5d4d8fd15449520
m305__41\m305 <coldogt@gmail.com>
2024-01-06 17:37:33 +0800

    2024/1/6

--- Assets/Scripts/EnemyScripts/EnemyControl.cs
+++ Assets/Scripts/EnemyScripts/EnemyControl.cs
@@ -10,18 +10,28 @@ public class EnemyControl : MonoBehaviour
     public EnemyMoveMode moveMode;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float detectRange = 3f;
+
+    public float yOriginPos;
+    [Header("來回移動")]
+    //bool isToPoint1 = false;
+    //[SerializeField] float time = 1;
+    //public Transform Pivot_Environment;
+    //public Vector3 wayPoint1;
+    //public Vector3 wayPoint2;
+
     void Start()
     {
+        yOriginPos = transform.position.y;
         MoveTarget = GameObject.Find("Player");
     }
-    void Update()
+    //void Update()
+    //{
+    //    Move();
+    //}
+    private void FixedUpdate()
     {
         Move();
     }
-    // private void FixedUpdate()
-    // {
-    //     Move();
-    // }
     public void TakeDamage(int damageNum)
     {
         if (EnemyHP - damageNum > 0)
@@ -48,6 +58,19 @@ public class EnemyControl : MonoBehaviour
         {
             MoveModeList.KeepAway(MoveTarget, this.gameObject, detectRange, moveSpeed);
         }
+        else if (moveMode == EnemyMoveMode.BackAndForth)
+        {
+            MoveModeList.BackAndForth( this.gameObject, moveSpeed,yOriginPos);
+        }
         // 甚麼移動方式 就調用甚麼移動方法
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(Pivot_Environment.transform.position + wayPoint1, 0.3f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(Pivot_Environment.transform.position + wayPoint2, 0.3f);
+
     }
 }
--- Assets/Scripts/EnemyScripts/MoveModeList.cs
+++ Assets/Scripts/EnemyScripts/MoveModeList.cs
@@ -6,11 +6,13 @@ public enum EnemyMoveMode
 {
     MoveToward,
     KeepAway,
+    BackAndForth,
     StayStill,
     Drift,
 }
 public class MoveModeList
 {
+
     static public void MoveToward(GameObject target, GameObject enemy, float range, float moveSpeed)
     {
         float distance = Vector3.Distance(target.transform.position, enemy.transform.position);
@@ -30,8 +32,11 @@ public class MoveModeList
             // Vector3 dir = target.transform.position - enemy.transform.position;
             Debug.Log("Enemy keep away from player.");
             enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, target.transform.position, -moveSpeed * Time.deltaTime);
-
         }
+    }
+    static public void BackAndForth(GameObject enemy, float moveSpeed,float yOrigin)
+    {
+        enemy.transform.position = new Vector3(Mathf.PingPong(Time.time * moveSpeed, 5), yOrigin, 0);
     }
     static public void StayStill(GameObject target, GameObject enemy, float range, float moveSpeed)
     {
